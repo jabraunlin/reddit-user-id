5e126a9262d332e4e30347beca46cefc206e9def
John Braunlin <John@MacBook-Pro-5.local>
2018-11-10 12:49:17 -0800

    word distributions

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,14 @@
 import pyspark as ps
 from pyspark.sql import functions as F
 from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType, FloatType, IntegerType
+from pyspark.sql.types import StringType, FloatType, IntegerType, ArrayType
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.feature import CountVectorizer, Tokenizer
 from pyspark.ml.feature import StopWordsRemover
 
-
+# If you need to put a local file onto worker nodes:
+# hadoop fs -put filename /user/hadoop/file.csv
 spark = (
     ps.sql.SparkSession.builder
     .master("local[4]")
@@ -30,5 +31,17 @@ df_join_comments = comments.withColumn('corpus', join_comments_udf(comments['col
 tokenizer = Tokenizer(inputCol="corpus", outputCol="words")
 tokens = tokenizer.transform(df_join_comments)
 # Drop stop words, create new df called words
-stopwords = StopWordsRemover(inputCol="words", outputCol="true_words")
-words = stopwords.transform(tokens)
+# stopwords = StopWordsRemover(inputCol="words", outputCol="true_words")
+# words = stopwords.transform(tokens)
+
+
+# Mendenhall's Characteristic Curves of Composition
+def word_length(words):
+    return [len(word) for word in words]
+
+
+word_length_udf = udf(word_length, ArrayType(IntegerType()))
+word_length_df = tokens.withColumn('word_lengths', word_length_udf(tokens['words']))
+total_words_udf = udf(lambda x: len(x), IntegerType())
+total_words_df = word_length_df.withColumn('total_words', word_length_udf(word_length_df['words']))
+plot_df = total_words_df.orderBy(('total_words'), ascending=False).limit(10)
