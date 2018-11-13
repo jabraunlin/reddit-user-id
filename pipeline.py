0cd456f30899fa5deb5b3757015fb7d7d099ac4f
John Braunlin <John@MacBook-Pro-5.local>
2018-11-12 21:33:58 -0800

    nltk cleaning

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,4 @@
-    import pyspark as ps
+import pyspark as ps
 from pyspark.sql import functions as F
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType, FloatType, IntegerType, ArrayType
@@ -18,7 +18,7 @@ import numpy as np
 spark = (
     ps.sql.SparkSession.builder
     .master("local[4]")
-    .appName("lecture")
+    .appName("project")
     .getOrCreate()
 )
 
@@ -27,26 +27,181 @@ sc = spark.sparkContext
 
 df = spark.read.json("sample.json")
 
-df1 = df[['author', 'body', 'subreddit']]
-df_users = df1.groupby('author', 'subreddit').agg(
+df1 = df[['author', 'body']]
+df_users = df1.groupby('author').agg(
     F.count('body')).orderBy('count(body)', ascending=False)
-comments = df.groupBy("author", 'subreddit').agg(F.collect_list("body"))
+comments = df.groupBy("author").agg(F.collect_list("body"))
 join_comments_udf = udf(lambda x: ' '.join(x), StringType())
 df_join_comments = comments.withColumn('corpus', join_comments_udf(comments['collect_list(body)']))
-#Count and drop hyperlinks
-def count_links(s):    
-    num_links = len(re.findall(r'\(http.+\)',s)[0].split(')('))
+# Count and drop hyperlinks
+
+
+def count_links(s):
+    num_links = len(re.findall(r'\(http.+\)', s)[0].split(')('))
     return num_links
+
+
 count_links_udf = udf(count_links, IntegerType())
-df_join_comments = df_join_comments.withColumn('link_count', count_links_udf(df_join_comments['corpus']))
+df_join_comments = df_join_comments.withColumn(
+    'link_count', count_links_udf(df_join_comments['corpus']))
+
+
 def drop_links(s):
-    return re.sub(r'\(http.+\)','',s)
+    return re.sub(r'\(http.+\)', '', s)
+
+
 drop_links_udf = udf(drop_links, StringType())
 df_join_comments = df_join_comments.withColumn('corpus', drop_links_udf(df_join_comments['corpus']))
 
+
+def count_bold(s):
+    x = 0
+    y = 0
+    try:
+        x = len(re.findall(r'**.+**', s)[0].split(')('))
+    except:
+        x = 0
+    try:
+        y = len(re.findall(r'__.+__', s)[0].split(')('))
+    except:
+        y = 0
+    return x+y
+
+
+count_bold_udf = udf(count_bold, IntegerType())
+df_count_bold = df_drop_links.withColumn('bold_count', count_bold_udf(df_drop_links['corpus']))
+
+
+def drop_bold(s):
+    try:
+        return re.sub(r'**', '', s)
+    except:
+        try:
+            return re.sub(r'__', '', s)
+        except:
+            return s
+
+
+drop_bold_udf = udf(drop_bold, StringType())
+df_drop_bold = df_count_bold.withColumn('corpus', drop_bold_udf(df_count_bold['corpus']))
+
+
+def count_italics(s):
+    x = 0
+    y = 0
+    try:
+        x = len(re.findall(r'*', s)[0].split('*'))
+    except:
+        x = 0
+    try:
+        y = len(re.findall(r'_', s)[0].split('_'))
+    except:
+        y = 0
+    return x+y
+
+
+count_italics_udf = udf(count_italics, IntegerType())
+df_count_italics = df_drop_bold.withColumn(
+    'italics_count', count_italics_udf(df_drop_bold['corpus']))
+
+
+def drop_italics(s):
+    try:
+        return re.sub(r'*', '', s)
+    except:
+        try:
+            return re.sub(r'_', '', s)
+        except:
+            return s
+
+
+drop_italics_udf = udf(drop_italics, StringType())
+df_drop_italics = df_count_italics.withColumn(
+    'corpus', drop_italics_udf(df_count_italics['corpus']))
+
+
+def count_blocks(s):
+    try:
+        return len(re.findall(r'    ', s)[0].split('    '))
+    except:
+        return 0
+
+
+count_blocks_udf = udf(count_blocks, IntegerType())
+df_count_blocks = df_drop_italics.withColumn(
+    'block_count', count_blocks_udf(df_drop_italics['corpus']))
+
+
+def drop_blocks(s):
+    try:
+        return re.sub(r'    ', '', s)
+    except:
+        return s
+
+
+drop_blocks_udf = udf(drop_blocks, StringType())
+df_drop_blocks = df_count_blocks.withColumn(
+    'corpus', drop_blocks_udf(df_count_blocks['corpus']))
+
+
+def count_headlines(s):
+    try:
+        return len(re.findall(r'##', s)[0].split('##'))
+    except:
+        return 0
+
+
+count_headlines_udf = udf(count_headlines, IntegerType())
+df_count_headlines = df_drop_blocks.withColumn(
+    'headline_count', count_headlines_udf(df_drop_blocks['corpus']))
+
+
+def drop_headlines(s):
+    try:
+        return re.sub(r'##', '', s)
+    except:
+        return s
+
+
+drop_headlines_udf = udf(drop_headlines, StringType())
+df_drop_headlines = df_count_headlines.withColumn(
+    'corpus', drop_headlines_udf(df_count_headlines['corpus']))
+
+
+def count_newlines(s):
+    try:
+        return len(re.findall(r'\n\n', s)[0].split('\n\n'))
+    except:
+        return 0
+
+
+count_newlines_udf = udf(count_newlines, IntegerType())
+df_count_newlines = df_drop_headlines.withColumn(
+    'newline_count', count_newlines_udf(df_count_newlines['corpus']))
+
+
+def drop_newlines(s):
+    try:
+        return re.sub(r'\n', '', s)
+    except:
+        return s
+
+
+drop_newlines_udf = udf(drop_newlines, StringType())
+df_drop_newlines = df_count_newlines.withColumn(
+    'corpus', drop_newlines_udf(df_count_newlines['corpus']))
+
+
+def drop_punct(s):
+    return re.sub(r'[^0-9a-zA-Z]', '', s)
+
+
+drop_punct_udf = udf(drop_punct, StringType())
+df_drop_punct = df_drop_newlines.withColumn('corpus1', df_drop_newlines['corpus'])
+
 # Tokenize words, create new df called tokens
-tokenizer = Tokenizer(inputCol="corpus", outputCol="words")
-tokens = tokenizer.transform(df_join_comments)
+tokenizer = Tokenizer(inputCol="corpus1", outputCol="words")
+tokens = tokenizer.transform(df_drop_punct)
 # Drop stop words, create new df called words
 # stopwords = StopWordsRemover(inputCol="words", outputCol="true_words")
 # words = stopwords.transform(tokens)
@@ -62,6 +217,7 @@ word_length_df = tokens.withColumn('word_lengths', word_length_udf(tokens['words
 total_words_udf = udf(lambda x: len(x), IntegerType())
 total_words_df = word_length_df.withColumn('total_words', word_length_udf(word_length_df['words']))
 plot_df = total_words_df.orderBy(('total_words'), ascending=False).limit(10)
+
 
 # plot the Curves of Composition
 MCCC = plot_df.select('author', 'word_lengths', 'total_words')
